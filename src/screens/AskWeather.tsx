2fff485900213128c192e92c02575b7014fb3dd8
yeopyeop-82 <a01055143647@gmail.com>
2024-04-04 21:58:27 +0900

    feat: react-native-radio-buttons-group 설치

--- src/screens/AskWeather.tsx
+++ src/screens/AskWeather.tsx
@@ -21,6 +21,25 @@ const Bold = styled.Text`
   font-weight: 800;
 `;
 
+const AskWeatherButtonWrapper = styled.View`
+  flex: 0.6;
+  width: 60%;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+const AskWeatherButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  flex: 0.12;
+  background-color: red;
+`;
+
+const AskWeatherButtonText = styled.Text`
+  font-size: 15px;
+`;
+
 const AskWeather = () => {
   return (
     <Wrapper>
@@ -30,6 +49,11 @@ const AskWeather = () => {
         </AskTitle>
         <AskTitle>어떠셨나요?</AskTitle>
       </AskTitleWrapper>
+      <AskWeatherButtonWrapper>
+        <AskWeatherButton>
+          <AskWeatherButtonText>추웠어요</AskWeatherButtonText>
+        </AskWeatherButton>
+      </AskWeatherButtonWrapper>
     </Wrapper>
   );
 };
