a54e2e12767c479c0da6b7943891eeb4461f5dcc
Kim Seung-yeop <a01055143647@gmail.com>
2024-07-08 15:23:15 +0900

    feat: 임시 비밀번호 발급 api 연결

--- src/screens/LoginReset.tsx
+++ src/screens/LoginReset.tsx
@@ -117,16 +117,41 @@ const LoginReset = () => {
     }
   };
 
-  const handleSubmit = () => {
-    if (email !== 'waither@example.com') {
-      setEmailMessage('등록된 이메일이 없습니다. 이메일을 다시 확인해주세요.');
-      setIsEmail(false);
-    } else {
-      showToast(
-        '가입된 이메일로 임시 비밀번호를 전송했어요',
-        '메일함을 확인해주세요',
+  const handleSubmit = async () => {
+    if (!isEmail) {
+      setEmailMessage('유효한 이메일을 입력해주세요.');
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://waither.shop/user/emails/temporary-password?email=${encodeURIComponent(
+          email,
+        )}`,
+        {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+          },
+        },
       );
-      navigation.navigate('EmailLogin');
+
+      if (response.ok) {
+        showToast(
+          '가입된 이메일로 임시 비밀번호를 전송했어요',
+          '메일함을 확인해주세요',
+        );
+        navigation.navigate('EmailLogin');
+      } else {
+        setEmailMessage(
+          '등록된 이메일이 없습니다. 이메일을 다시 확인해주세요.',
+        );
+        setIsEmail(false);
+      }
+    } catch (error) {
+      console.error('Error during request:', error);
+      setEmailMessage('오류가 발생했습니다. 다시 시도해주세요.');
+      setIsEmail(false);
     }
   };
 
